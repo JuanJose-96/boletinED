3cc293374d8739d449c63b10ccd14e6ba8b3da0b
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2025-02-19 22:48:01 +0100

    Comentarios y documentacion Javadoc de la clase Usuario junto con sus atributos y metodos

--- src/aulaVirtual/Usuario.java
+++ src/aulaVirtual/Usuario.java
@@ -10,9 +10,9 @@ import java.util.List;
  * @version 0.1
  */
 public class Usuario {
-    protected String nombre;
-    protected String email;
-    private List<Asignatura> asignaturas;
+    protected String nombre; //nombre del usuario
+    protected String email; // email del usuario
+    private List<Asignatura> asignaturas; // lista de asignaturas
 
     /**
      * Constructor de la clase Usuario
@@ -27,22 +27,44 @@ public class Usuario {
         this.asignaturas = new ArrayList<>();
     }
 
+
+    /**
+     * Getter de lista de asignaturas
+     * @return copia de la lista de las asignaturas
+     */
     public List<Asignatura> getAsignaturas() {
         return new ArrayList<>(asignaturas);
     }
 
+    /**
+     * Metodo para añadir una asignatura a la lista de asignaturas para cursar
+     * @param asignatura asignatura
+     */
     public void inscribirAsignatura(Asignatura asignatura) {
         asignaturas.add(asignatura);
     }
 
+    /**
+     * Getter del nombre del usuario
+     * @return nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Getter del email del usuario
+     * @return email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Metodo para que se inicie sesion en el aula virtual
+     * @param password recibe una cadena de caracteres que será la contraseña para entrar
+     * @return true
+     */
     public boolean iniciarSesión (String password) {
         // simula la consulta a la base de datos de las credenciales del usuario
         int hash = password.hashCode();
