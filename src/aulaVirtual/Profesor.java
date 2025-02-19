84eefcb4c147c3b53e4470239ac3a1028f1c9c0f
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2025-02-19 23:17:22 +0100

    Comentarios y documentacion Javadoc de la clase Profesor junto con sus atributos y metodos

--- src/aulaVirtual/Profesor.java
+++ src/aulaVirtual/Profesor.java
@@ -4,12 +4,28 @@ package aulaVirtual;
  * Representa a un profesor del aula virtual.
  * Un profesor puede impartir varias asignaturas.
  */
+
 public class Profesor extends Usuario {
+    /**
+     * Constructor de la clase profesor
+     * @param nombre nombre del profesor
+     * @param email email del profesor
+     */
     public Profesor(String nombre, String email) {
-        super(nombre, email);
+        super(nombre, email); // Llamada al constructor padre de la clase usuario
     }
 
 
+    /**
+     * Metodo del profesor para calificar a un alumno que cursa una asignatura y
+     * la nota de dicha asignatura. Si el profesor no imparte la asignatura se imprimirá
+     * que el profesor no imparte la asignatura
+     * @param alumno alumno al que se va a calificar
+     * @param asignatura asignatura que el alumno cursó
+     * @param nota nota que sacó el alumno en dicha asignatura
+     * @return true si imparte la asignatura
+     * @return false no imparte la asignatura
+     */
     public boolean calificarAlumno(Alumno alumno, Asignatura asignatura, int nota) {
         if (getAsignaturas().contains(asignatura)) {
             alumno.asignarNota(asignatura, nota);
